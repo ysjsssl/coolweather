939078ae6c982d6f000b3d53f973289d85b2f445
hp <hp@local.com>
2016-01-25 12:12:55 +0800

    添加数据库

---
+++ app/src/main/java/com/coolweather/app/db/CoolWeatherOpenHelper.java
@@ -0,0 +1,48 @@
+package com.coolweather.app.db;
+
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by hp on 2016/1/25.
+ */
+public class CoolWeatherOpenHelper extends SQLiteOpenHelper{
+
+    public static final String CREATE_PROVICE = "create table Province (" +
+            "id integer primary key autoincrement, " +
+            "province_name text, " +
+            "province_code text)";
+
+    public static final String CREATE_CITY = "create table City (" +
+            "id integer primary key autoincrement, " +
+            "city_name text, " +
+            "city_code text. " +
+            "province_id integer)";
+
+    public static final String CREATE_COUNTY = "create table County (" +
+            "id integer primary key autoincrement, " +
+            "county_name text, " +
+            "county_code text, " +
+            "city_id integer)";
+
+    public CoolWeatherOpenHelper(Context context, String name,
+                                 SQLiteDatabase.CursorFactory factory,
+                                 int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_PROVICE);
+        db.execSQL(CREATE_CITY);
+        db.execSQL(CREATE_COUNTY);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+}
---
+++ app/src/main/java/com/coolweather/app/model/Province.java
@@ -0,0 +1,27 @@
+package com.coolweather.app.model;
+
+/**
+ * Created by hp on 2016/1/25.
+ */
+public class Province {
+
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+}
