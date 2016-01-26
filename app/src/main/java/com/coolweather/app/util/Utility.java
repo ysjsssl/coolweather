9e00080afeb8912eba28cdb74c2db74f81e6434a
hp <hp@local.com>
2016-01-26 16:58:55 +0800

    添加布局及主活动

---
+++ app/src/main/java/com/coolweather/app/util/Utility.java
@@ -0,0 +1,67 @@
+package com.coolweather.app.util;
+
+import android.text.TextUtils;
+
+import com.coolweather.app.model.City;
+import com.coolweather.app.model.CoolWeatherDB;
+import com.coolweather.app.model.County;
+import com.coolweather.app.model.Province;
+
+/**
+ * Created by hp on 2016/1/26.
+ */
+public class Utility {
+
+    public synchronized static boolean handleProvinceResponse(CoolWeatherDB coolWeatherDB, String response) {
+        if (!TextUtils.isEmpty(response)) {
+            String[] allProvince = response.split(",");
+            if (allProvince != null && allProvince.length > 0) {
+                for (String p : allProvince) {
+                    String[] array = p.split("\\|");
+                    Province province = new Province();
+                    province.setProvinceCode(array[0]);
+                    province.setProvinceName(array[1]);
+                    coolWeatherDB.saveProvince(province);
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean handleCitiesResponse(CoolWeatherDB coolWeatherDB, String response, int provinceId) {
+        if (!TextUtils.isEmpty(response)) {
+            String[] allCities = response.split(",");
+            if (allCities != null && allCities.length > 0) {
+                for (String c : allCities) {
+                    String[] array = c.split("\\|");
+                    City city = new City();
+                    city.setCityCode(array[0]);
+                    city.setCityName(array[1]);
+                    city.setProvinceId(provinceId);
+                    coolWeatherDB.saveProvince(city);
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean handleCountiesResponse(CoolWeatherDB coolWeatherDB, String response, int cityId) {
+        if (!TextUtils.isEmpty(response)) {
+            String[] allCounties = response.split(",");
+            if (allCounties != null && allCounties.length > 0) {
+                for (String c : allCounties) {
+                    String[] array = c.split("\\|");
+                    County county = new County();
+                    county.setCountyCode(array[0]);
+                    county.setCountyName(array[1]);
+                    county.setCityId(cityId);
+                    coolWeatherDB.saveCounty(county);
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+}
