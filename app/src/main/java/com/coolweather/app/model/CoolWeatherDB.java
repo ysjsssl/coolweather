16c324e95d50c1c3196784814a4601d6de718fff
hp <hp@local.com>
2016-02-02 17:25:42 +0800

    加入显示天气信息功能

--- app/src/main/java/com/coolweather/app/model/CoolWeatherDB.java
+++ app/src/main/java/com/coolweather/app/model/CoolWeatherDB.java
@@ -96,7 +96,7 @@ public class CoolWeatherDB {
     public void saveCounty(County county) {
         if (county != null) {
             ContentValues values = new ContentValues();
-            values.put("count_name", county.getCountyName());
+            values.put("county_name", county.getCountyName());
             values.put("county_code", county.getCountyCode());
             values.put("city_id", county.getCityId());
             db.insert("County", null, values);
